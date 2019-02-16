e803b7abf2fc46371f5078487b6e45fec851a521
Shoedo Nonmouse <shoedo@example.com>
2019-02-16 11:58:03 -0800

    navigation drawer for mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 (function () {
 
+    var $ = {
+        toggle: document.querySelector('.site-nav-toggle a'),
+        close: document.querySelector('.site-nav-close a'),
+        body: document.querySelector('body')
+    };
+    
+    $.toggle.addEventListener('click', () => {
+        console.log('whattt')
+        $.body.classList.toggle('drawer-open');
+    });
+
+    $.close.addEventListener('click', () => {
+        $.body.classList.remove('drawer-open');
+    });
 })();
